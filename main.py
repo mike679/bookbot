1e368581ad1c7dbe4cbf4f7266df14ee17789bf9
mike679 <mdevasia2000@gmail.com>
2025-01-31 01:53:47 +0000

    a program which returns the occurence of the characters in the novel

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     words = file_contents.split()
-    print(len(words))    
+    print(f"{len(words)} words found in the document")
+    
+    file_contents_char_dict=char_count(file_contents)
+    for char in file_contents_char_dict:
+        if char.isalpha():
+            print(f"The '{char}' character was found {file_contents_char_dict[char]} times")
 
+
+def char_count(file_contents):
+    file_contents_char_dict = {}
+    file_contents_char_list = list(file_contents.lower())
+    file_contents_char_set = set(file_contents_char_list)
+    file_contents_char_unique = list(file_contents_char_set)
+    for char in file_contents_char_list:
+        if char in file_contents_char_dict:
+            file_contents_char_dict[char]+=1
+        else:
+            file_contents_char_dict[char]=1
+    return file_contents_char_dict
 main()
